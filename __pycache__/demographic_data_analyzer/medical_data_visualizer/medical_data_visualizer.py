df1e769bc9aa7d6dd7e89a2463f35aeb0a12b2b1
Shikha <149319456+shikha1603@users.noreply.github.com>
2026-04-06 14:00:08 +0530

    Add files via upload

---
+++ __pycache__/demographic_data_analyzer/medical_data_visualizer/medical_data_visualizer.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = pd.read_csv('medical_examination.csv')
+
+# 2
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
+
+# 3
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=sorted(['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']))
+
+    # 6
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    
+
+    # 7
+    g = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar')
+
+    # 8
+    fig = g.fig
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    # 12
+    corr = df_heat.corr()
+
+    # 13
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+
+    # 14
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    # 15
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, vmax=0.32, cbar_kws={'shrink': 0.5}, square=True, linewidths=0.5)
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
